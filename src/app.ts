68374c2e3baf99cafe099c7b7240a661b999c99f
Alexander Arrico <alex@hourwork.com>
2022-07-20 10:27:29 -0700

    use bodyparser package

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
 const app: Application = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async(req: Request, res: Response): Promise<Response> => {
     return res.status(200).send({ message: 'Hello!'});
