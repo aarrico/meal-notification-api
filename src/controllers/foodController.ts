94ed878a8171ca7017de41bf62a2f9359ef1e3bb
Alexander Arrico <alex@hourwork.com>
2022-07-20 17:26:24 -0700

    update food controller for typeorm

--- src/controllers/foodController.ts
+++ src/controllers/foodController.ts
@@ -1,48 +1,74 @@
-import { BaseFood, Food } from "../models/food";
-import db from "../db/db";
+import { Request, Response } from "express";
+import { Food } from "../entities/food.entity";
+import { db } from "../db/db";
+import { Repository } from "typeorm";
 
-export const findAll = async (): Promise<Food[]> => {
-  const queryStr = `SELECT * FROM food ORDER BY name DESC`;
+class FoodController {
+  private foodRepo: Repository<Food>;
 
-  try {
-    const res = await db.query(queryStr);
-    return res.rows;
-  } catch (err) {
-    throw err;
-  }
-}
-
-export const find = async (id: number): Promise<Food> => {
-  const queryStr = `SELECT * FROM food WHERE id = ${id}`;
-
-  try {
-    const res = await db.query(queryStr);
-    return res.rows[0];
-  } catch (err) {
-    throw err;
-  }
-}
-
-export const create = async (food: BaseFood): Promise<number> => {
-  const queryStr = `INSERT INTO food(name, protein, carb, fat) VALUES(${food.name}, ${food.protein}, ${food.carb}, ${food.fat})`;
-
-  let id: number;
-  try {
-    const res = await db.query(queryStr);
-    id = res.rows[0].id;
-  } catch (err) {
-    throw err;
+  constructor() {
+    this.foodRepo = db.getRepository(Food);
   }
 
-  return id;
-};
+  public findAll = async (req: Request, res: Response) => {
+    try {
+      const foods = await this.foodRepo.find();
+      return res.send(foods);
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  }
 
-export const remove = async (id: number): Promise<void> => {
-  const queryStr = `DELETE FROM food WHERE id = ${id}`;
+  public findById = async (req: Request, res: Response) => {
+    const id = req.params.id;
 
-  try {
-    await db.query(queryStr);
-  } catch (err) {
-    throw err;
+    try {
+      const food = await this.foodRepo.findOneByOrFail({ id: id });
+      return res.send(food);
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  }
+
+  public findByName = async (req: Request, res: Response) => {
+    const name = req.params.name;
+
+    try {
+      const food = await this.foodRepo.findOneByOrFail({ name: name });
+      return res.send(food);
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  }
+
+  public create = async (req: Request, res: Response) => {
+    const { name, protein, carb, fat } = req.body;
+    const food = new Food();
+    food.name = name;
+    food.protein = protein;
+    food.carb = carb;
+    food.fat = fat;
+
+    try {
+      await this.foodRepo.save(food);
+    } catch (err) {
+      res.status(400).send('Food already exists!');
+      return;
+    }
+
+    return res.status(201).send(`Food ${name} created`);
+  }
+
+  public delete = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    
+    try {
+      await this.foodRepo.findOneByOrFail({ id: id });
+    } catch (err) {
+      res.status(404).send('Food not found!');
+      return;
+    }
+
+    res.status(204).send();
   }
 }
